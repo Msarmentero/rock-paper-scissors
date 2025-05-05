140d632212927dd465fcd0616b649403a5ed15a2
Msarmentero <msarmentero98@gmail.com>
2025-05-05 19:24:35 -0300

    add human player logic Add logic to compare choices and determine round winner Make the game play for 5 rounds Declare winner based on score after 5 rounds

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,30 +1,77 @@
-function getRandomValues(min, max){
-    return Math.floor(Math.random() * (max - min) + min);
-}
+function playGame(){
+    let humanScore= 0;
+    let computerScore= 0;
+    let humanChoice = "";
+    let computerChoice = "";
 
-function getComputerChoice(){
-    let computerChoice = ""
-    const computerValue = getRandomValues(1,4)
-    console.log(computerValue)
-
-    if (computerValue === 1){
-        computerChoice = "scissors"
-        console.log(computerChoice)
+    function getRandomValues(min, max){
+        return Math.floor(Math.random() * (max - min) + min);
     }
-    if (computerValue === 2){
-        computerChoice = "rock"
-        console.log(computerChoice)
+    
+    function getComputerChoice(){
+        let computerChoice = ""
+        const computerValue = getRandomValues(1,4)
+        
+    
+        if (computerValue === 1){
+            computerChoice = "scissors"
+        
+        }
+        if (computerValue === 2){
+            computerChoice = "rock"
+            
+        }
+        if (computerValue === 3){
+            computerChoice = "paper"
+        
+        }
+        return computerChoice
     }
-    if (computerValue === 3){
-        computerChoice = "paper"
-        console.log(computerChoice)
+
+    function getHumanChoice(){
+        const humanChoice = window.prompt("Rock, paper or scissors?")
+        return humanChoice
+        
     }
-}
 
-function getHumanChoice(){
-    const humanChoice = window.prompt("Rock, paper or scissors?")
-    console.log(humanChoice)
-}
-getHumanChoice();
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        
+        //console.log(`human choice: ${humanChoice}`)
+        //console.log(`computer choice: ${computerChoice}`)
+        
+        if(humanChoice === computerChoice){
+            console.log("Draw!")
+        } else if(
+            (humanChoice === "rock" && computerChoice ==="scissors") ||
+            (humanChoice === "paper" && computerChoice ==="rock") ||
+            (humanChoice === "scissors" && computerChoice ==="paper")
+        ){
+            console.log("Human wins!")
+            humanScore++;
+        } else{
+            console.log("computer wins!")
+            computerScore++;
+        }
+        console.log(`Score, human score: ${humanScore}, computer score: ${computerScore}`)
+        }
 
-getComputerChoice();
+
+    for (let i = 0; i < 5; i++) {
+        humanChoice= getHumanChoice();
+        computerChoice= getComputerChoice();
+
+        playRound(humanChoice,computerChoice);
+        
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Human wins the game!");
+      } else if (computerScore > humanScore) {
+        console.log("Computer wins the game!");
+      } else {
+        console.log("It's a draw!");
+      }
+}
+playGame()
+
