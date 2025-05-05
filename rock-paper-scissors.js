59dcdba50b189b73f19ccacb161b34ceb5f3b3e2
Msarmentero <msarmentero98@gmail.com>
2025-05-05 17:55:02 -0300

    Provide basic logic for computer player choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,24 @@
-console.log("Hello again worlds")
+function getRandomValues(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getComputerChoice(){
+    let computerChoice = ""
+    const computerValue = getRandomValues(1,4)
+    console.log(computerValue)
+
+    if (computerValue === 1){
+        computerChoice = "scissors"
+        console.log(computerChoice)
+    }
+    if (computerValue === 2){
+        computerChoice = "rock"
+        console.log(computerChoice)
+    }
+    if (computerValue === 3){
+        computerChoice = "paper"
+        console.log(computerChoice)
+    }
+}
+
+getComputerChoice();
